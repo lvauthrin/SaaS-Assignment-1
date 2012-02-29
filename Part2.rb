92b91b9ec2921bfa3f9011a73080ea86332f6dc6
Laurent Vauthrin <lvauthrin@gmail.com>
2012-02-29 14:50:34 -0800

    Removed blank README

--- Part2.rb
+++ Part2.rb
@@ -1,26 +1,32 @@
 class WrongNumberOfPlayersError < StandardError ; end
 class NoSuchStrategyError < StandardError ; end
 
-def rps_game_winner(game)
+def rps_game_winner(game)  
   raise WrongNumberOfPlayersError unless game.length == 2
   strategyMappings = { "R" => "S", "S" => "P", "P" => "R" }
-  firstStrategy = game[0][1]
-  secondStrategy = game[1][1]
-  raise NoSuchStrategyError unless strategyMappings.keys.include?(firstStrategy) or strategyMappings.keys.include?(secondStrategy)
-  return game[firstStrategy.eql?(secondStrategy) or strategyMappings[firstStrategy].eql?(secondStrategy) ? 0 : 1]
+  game.each{ |player| raise NoSuchStrategyError unless strategyMappings.keys.include?(player[1]) }  
+  game.sort { |a, b| 
+    if a[1] == b[1]
+      comp = 0
+    elsif strategyMappings[a[1]] == b[1]
+      comp = -1
+    else
+      comp = 1
+    end 
+  }[0]
 end
 
-puts rps_game_winner([ [ "Armando", "P" ], [ "Dave", "S" ] ]) 
+#puts rps_game_winner([ [ "Armando", "P" ], [ "Dave", "S" ] ]).to_s
 
 def rps_tournament_winner(tournament)
-  runningTournament = tournament
-  while (runningTournament.length > 1) 
-    runningTournament.map(|
+  runningTournament = tournament.flatten(1)
+  while runningTournament.length > 1 
+    runningTournament = runningTournament.map { |game| puts game.to_s; rps_game_winner(game) }
   end
   return runningTournament[0]
 end
 
-rps_tournament_winner([
+puts rps_tournament_winner([
 [
 [ ["Armando", "P"], ["Dave", "S"] ],
 [ ["Richard", "R"],  ["Michael", "S"] ],
