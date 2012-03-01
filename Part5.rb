d1cd92475fda50ea64bcb4cd7eafe16458f96474
Laurent Vauthrin <lvauthrin@gmail.com>
2012-03-01 12:15:26 -0800

    Updated part5.rb to conform to HW assignment specification

--- Part5.rb
+++ Part5.rb
@@ -5,16 +5,9 @@ class Class
   attr_reader attr_name+"_history" # create bar_history getter
   class_eval %Q{ 
    
-   alias_method :old_initialize, :initialize
-   
-   def initialize
-     old_initialize
-     @#{attr_name}_history = Array.new
-     @#{attr_name}_history.push(nil)
-   end
-   
    def #{attr_name}=(val) 
      @#{attr_name} = val
+     @#{attr_name}_history = [nil] if @#{attr_name}_history.nil?
      @#{attr_name}_history.push(val)
    end
     
@@ -24,12 +17,15 @@ end
 
 class Foo
  attr_accessor_with_history :bar
+ attr_accessor_with_history :baz
 end
 
-# f = Foo.new
-# f.bar = 1
-# f.bar = 2
-# puts f.bar_history.to_s
-# f = Foo.new
-# f.bar = 4
-# puts f.bar_history.to_s
+f = Foo.new
+f.bar = 1
+f.bar = 2
+puts f.bar_history.to_s
+f = Foo.new
+f.bar = 4
+f.baz = 5
+puts f.bar_history.to_s
+puts f.baz_history.to_s
